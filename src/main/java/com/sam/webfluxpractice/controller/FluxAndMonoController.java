4f84b0e02f556f42d6f32c317076903dd73b4f43
Samim <samimaftabahmed@gmail.com>
2020-05-20 20:51:24 +0530

    api for implementation of flux and flux stream

---
+++ src/main/java/com/sam/webfluxpractice/controller/FluxAndMonoController.java
@@ -0,0 +1,28 @@
+package com.sam.webfluxpractice.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+@RestController
+public class FluxAndMonoController {
+
+    @GetMapping("/flux")
+    public Flux<Integer> fluxEndpoint() {
+
+        return Flux.just(1, 2, 3, 4)
+                .delayElements(Duration.ofSeconds(1))
+                .log();
+    }
+
+    @GetMapping(value = "/flux-stream", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    public Flux<Integer> fluxStreamEndpoint() {
+
+        return Flux.just(1, 2, 3, 4, 22, 6565, 8974654, 55, 12313, 77, 12)
+                .delayElements(Duration.ofSeconds(1))
+                .log();
+    }
+}
