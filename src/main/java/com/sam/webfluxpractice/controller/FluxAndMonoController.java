de228e61f66c59f8668aa4e3daa689e08aeb770a
Samim <samimaftabahmed@gmail.com>
2020-05-20 21:13:32 +0530

    api for implementation of mono

--- src/main/java/com/sam/webfluxpractice/controller/FluxAndMonoController.java
+++ src/main/java/com/sam/webfluxpractice/controller/FluxAndMonoController.java
@@ -4,6 +4,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 
@@ -23,6 +24,13 @@ public class FluxAndMonoController {
 
         return Flux.just(1, 2, 3, 4, 22, 6565, 8974654, 55, 12313, 77, 12)
                 .delayElements(Duration.ofSeconds(1))
+                .log();
+    }
+
+    @GetMapping("/mono")
+    public Mono<Integer> monoEndpoint() {
+
+        return Mono.just(1)
                 .log();
     }
 }
